68297452f035b38bfcc2e69efb9a0fa0afbf2bd6
Jasen Samuels <jasensamuels@gmail.com>
2021-03-14 05:22:02 -0400

    Update Script - v1.1
    1/2: Implemented new deletion option (you can opt to have vehicles deleted instead of killing the engine; useful for vehicles with weapons that still work with a dead engine).
    
    2/2: Changed the blacklist format to now use SpawnByName codes rather than hardcoded hashes, making this more user-friendly for people who wish to use this script.

--- VehicleKiller.lua
+++ VehicleKiller.lua
@@ -8,13 +8,14 @@
 local CheckNearby	= false	--[[ Set this to true if you want this script to check vehicles near players (including vehicles in use by players); Set this to false if you want this script to only check vehicles that players are currently using. ]]
 local AlwaysActive	= false	--[[ Set this to true if you want this script to always run regardless of the TriggerKey being pressed or not. ]]
 local RunWhileHeld	= false	--[[ Set this to true if you want this script to loop-run while the TriggerKey is being held down; Set this to false if you want this script to only run once for every one press of the TriggerKey. ]]
+local DeleteVehicle	= false --[[ Set this to true if you want this script to delete the vehicle; Set this to false if you want this script to kill the engine. ]]
 local MaxNearbyVehs	= 4		--[[ Can be any integer number above 0. This number is per player; the closest (4 by default) vehicles (that your game knows about) near players will be checked, if CheckNearby is set to true. ]]
 local TriggerKey	= 223	--[[ 223	INPUT_SCRIPT_RDOWN	LEFT MOUSE BUTTON	A | Default TriggerKey; https://docs.fivem.net/docs/game-references/controls/ for alternate keys. ]]
 
 
 
-local VehicleBlacklist	= { -- Format: '["VehicleHash"] = true,'
-	["2069146067"] = true,	--oppressor2 aka Oppressor MK2
+local VehicleBlacklist	= { -- Format: '"VehicleSpawnByNameCode",'
+	"oppressor2",	--Oppressor MK2
 }
 
 
@@ -24,22 +25,30 @@ local VehicleBlacklist	= { -- Format: '["VehicleHash"] = true,'
 --[[ Script/Code Area ]]
 local PAD = CONTROLS--Added/Used due to FiveM differences, for people who are more familiar with FiveM Lua scripting than GTA V Lua Plugin Scripting, same with a lot of the natives below.
 
+local GetHashKey					= GAMEPLAY.GET_HASH_KEY
 local IsControlPressed if RunWhileHeld then IsControlPressed = PAD.IS_CONTROL_PRESSED else IsControlPressed = PAD.IS_CONTROL_JUST_PRESSED end
 local NetworkIsPlayerActive			= NETWORK.NETWORK_IS_PLAYER_ACTIVE
 local GetVehiclePedIsUsing			= PED.GET_VEHICLE_PED_IS_USING
 local GetPlayerPed					= PLAYER.GET_PLAYER_PED
-local tostring						= tostring
 local GetEntityModel				= ENTITY.GET_ENTITY_MODEL
 local NetworkHasControlOfEntity		= NETWORK.NETWORK_HAS_CONTROL_OF_ENTITY
 local NetworkRequestControlOfEntity	= NETWORK.NETWORK_REQUEST_CONTROL_OF_ENTITY
+local SetEntityAsMissionEntity		= ENTITY.SET_ENTITY_AS_MISSION_ENTITY
+local DeleteEntity					= ENTITY.DELETE_ENTITY
 local SetVehicleEngineHealth		= VEHICLE.SET_VEHICLE_ENGINE_HEALTH
 
 
 
-local Vehicle
+local _VehicleBlacklist, Vehicle = {}, nil
 
 local VehicleKiller =
 {
+	init	=	function()
+					for i=1, #VehicleBlacklist do
+						_VehicleBlacklist[GetHashKey(VehicleBlacklist[i])] = true
+					end
+					VehicleBlacklist = nil
+				end,
 	tick	=	function()
 					if AlwaysActive or IsControlPressed(0, TriggerKey) then
 						--if CheckNearby then
@@ -48,9 +57,14 @@ local VehicleKiller =
 							for i=0,31 do
 								if NetworkIsPlayerActive(i) then
 									Vehicle = GetVehiclePedIsUsing(GetPlayerPed(i))
-									if VehicleBlacklist[tostring(GetEntityModel(Vehicle))] then
+									if _VehicleBlacklist[GetEntityModel(Vehicle)] then
 										if NetworkHasControlOfEntity(Vehicle) or NetworkRequestControlOfEntity(Vehicle) then
-											SetVehicleEngineHealth(Vehicle, -4000.0)
+											if DeleteVehicle then
+												SetEntityAsMissionEntity(Vehicle, true, true)
+												DeleteEntity(Vehicle)
+											else
+												SetVehicleEngineHealth(Vehicle, -4000.0)
+											end
 										end
 									end
 								end
