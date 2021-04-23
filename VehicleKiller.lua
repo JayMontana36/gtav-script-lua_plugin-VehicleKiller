810da8416eac3590d391c1524167d1320ef69749
Jasen Samuels <jasensamuels@gmail.com>
2021-04-23 02:48:51 -0400

    Update Script for JM36 LuaPlugin v3.0
    Make several changes to the script/code to make use of several features and functions specific to JM36 LuaPlugin v3.0
    
    Move/Export config to separate ini file
    
    Exempt player's vehicle from being affected by the script, so long as the player was using that vehicle last and prior to triggering the script.

--- VehicleKiller.lua
+++ VehicleKiller.lua
@@ -5,53 +5,44 @@
 
 
 --[[ Config Area ]]
-local CheckNearby	= false	--[[ Set this to true if you want this script to check vehicles near players (including vehicles in use by players); Set this to false if you want this script to only check vehicles that players are currently using. ]]
-local AlwaysActive	= false	--[[ Set this to true if you want this script to always run regardless of the TriggerKey being pressed or not. ]]
-local RunWhileHeld	= false	--[[ Set this to true if you want this script to loop-run while the TriggerKey is being held down; Set this to false if you want this script to only run once for every one press of the TriggerKey. ]]
-local DeleteVehicle	= false --[[ Set this to true if you want this script to delete the vehicle; Set this to false if you want this script to kill the engine. ]]
-local MaxNearbyVehs	= 4		--[[ Can be any integer number above 0. This number is per player; the closest (4 by default) vehicles (that your game knows about) near players will be checked, if CheckNearby is set to true. ]]
-local TriggerKey	= 223	--[[ 223	INPUT_SCRIPT_RDOWN	LEFT MOUSE BUTTON	A | Default TriggerKey; https://docs.fivem.net/docs/game-references/controls/ for alternate keys. ]]
-
-
-
-local VehicleBlacklist	= { -- Format: '"VehicleSpawnByNameCode",'
-	"oppressor2",	--Oppressor MK2
-}
+-- See VehicleKiller.ini file
 
 
 
 
 
 --[[ Script/Code Area ]]
-local PAD = CONTROLS--Added/Used due to FiveM differences, for people who are more familiar with FiveM Lua scripting than GTA V Lua Plugin Scripting, same with a lot of the natives below.
-
-local GetHashKey					= GAMEPLAY.GET_HASH_KEY
-local IsControlPressed if RunWhileHeld then IsControlPressed = PAD.IS_CONTROL_PRESSED else IsControlPressed = PAD.IS_CONTROL_JUST_PRESSED end
-local NetworkIsPlayerActive			= NETWORK.NETWORK_IS_PLAYER_ACTIVE
-local GetVehiclePedIsUsing			= PED.GET_VEHICLE_PED_IS_USING
-local GetPlayerPed					= PLAYER.GET_PLAYER_PED
-local GetPedNearbyVehicles			= PED.GET_PED_NEARBY_VEHICLES
-local GetEntityModel				= ENTITY.GET_ENTITY_MODEL
-local NetworkHasControlOfEntity		= NETWORK.NETWORK_HAS_CONTROL_OF_ENTITY
-local NetworkRequestControlOfEntity	= NETWORK.NETWORK_REQUEST_CONTROL_OF_ENTITY
-local SetEntityAsMissionEntity		= ENTITY.SET_ENTITY_AS_MISSION_ENTITY
-local DeleteEntity					= ENTITY.DELETE_ENTITY
-local SetVehicleEngineHealth		= VEHICLE.SET_VEHICLE_ENGINE_HEALTH
+local IsTriggerTrue
+local GetHashKey, NetworkIsPlayerActive, GetVehiclePedIsUsing, GetPlayerPed, GetPedNearbyVehicles, GetEntityModel, NetworkHasControlOfEntity, NetworkRequestControlOfEntity, SetEntityAsMissionEntity, DeleteEntity, SetVehicleEngineHealth
+	= GetHashKey, NetworkIsPlayerActive, GetVehiclePedIsUsing, GetPlayerPed, GetPedNearbyVehicles, GetEntityModel, NetworkHasControlOfEntity, NetworkRequestControlOfEntity, SetEntityAsMissionEntity, DeleteEntity, SetVehicleEngineHealth
 
 
 
 local _VehicleBlacklist, ActivePlayers, CheckedVehicles, Vehicle = {}, {}, nil, nil
-
-local VehicleKiller =
-{
+local CheckNearby, AlwaysActive, RunWhileHeld, DeleteVehicle, MaxNearbyVehs, TriggerKey
+return {
 	init	=	function()
-					for i=1, #VehicleBlacklist do
-						_VehicleBlacklist[GetHashKey(VehicleBlacklist[i])] = true
+					local Config = configFileRead("VehicleKiller.ini")
+					
+					local tonumber = tonumber
+					CheckNearby = Config.CheckNearby == "true"
+					AlwaysActive = Config.AlwaysActive == "true"
+					RunWhileHeld = Config.RunWhileHeld == "true"
+					if RunWhileHeld then IsTriggerTrue = IsControlPressed else IsTriggerTrue = IsControlJustPressed end
+					DeleteVehicle = Config.DeleteVehicle == "true"
+					MaxNearbyVehs = tonumber(Config.MaxNearbyVehs)
+					TriggerKey = tonumber(Config.TriggerKey)
+					
+					local tostring = tostring
+					local i = "1"
+					while Config[i] do
+						_VehicleBlacklist[GetHashKey(Config[i])] = true
+						i = tostring(i+1)
 					end
-					VehicleBlacklist = nil
 				end,
-	tick	=	function()
-					if AlwaysActive or IsControlPressed(0, TriggerKey) then
+	loop	=	function(Info)
+					if AlwaysActive or IsTriggerTrue(0, TriggerKey) then
+						local PlayersVehicle = Info.Player.Vehicle.Id
 						if CheckNearby then
 							CheckedVehicles = {}
 							
@@ -67,7 +58,7 @@ local VehicleKiller =
 									NearbyVehs, NearbyVehsNum = GetPedNearbyVehicles(ActivePlayers[i], MaxNearbyVehs)
 									for j=1,NearbyVehsNum do
 										Vehicle = NearbyVehs[j]
-										if not CheckedVehicles[Vehicle] then
+										if Vehicle ~= PlayersVehicle and not CheckedVehicles[Vehicle] then
 											if _VehicleBlacklist[GetEntityModel(Vehicle)] then
 												if NetworkHasControlOfEntity(NearbyVehs[j]) or NetworkRequestControlOfEntity(NearbyVehs[j]) then
 													if DeleteVehicle then
@@ -89,7 +80,7 @@ local VehicleKiller =
 							for i=0,31 do
 								if NetworkIsPlayerActive(i) then
 									Vehicle = GetVehiclePedIsUsing(GetPlayerPed(i))
-									if _VehicleBlacklist[GetEntityModel(Vehicle)] then
+									if Vehicle ~= PlayersVehicle and _VehicleBlacklist[GetEntityModel(Vehicle)] then
 										if NetworkHasControlOfEntity(Vehicle) or NetworkRequestControlOfEntity(Vehicle) then
 											if DeleteVehicle then
 												SetEntityAsMissionEntity(Vehicle, true, true)
@@ -105,4 +96,3 @@ local VehicleKiller =
 					end
 				end
 }
-return VehicleKiller
